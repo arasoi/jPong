fd5ec99c28211bb3b5c59acdf89ebcd0c47a7e95
Paul Saukas <arasoi@samurainews.com>
2022-02-27 17:18:57 -0500

    Added the basic redeing oif the ball and clenaed up some of the code.

--- src/main.js
+++ src/main.js
@@ -6,20 +6,25 @@ class jPong {
     }
 
     init(){
-        setInterval(this.draw(this.ctx, this.canvas), 10); // Here we setup the Game Loop. We will call the draw function at a set interval of 10 milliseconds. 
+        this.ball = new Ball(this.canvas)
+        this.draw()
     }
 
     /* Draw
         This is the main game loop. it is here that we will check all inputs, calc updates and render the next frame of the game
     */
-    draw (ctx, canvas) {
-        ctx.clearRect(0,0, this.canvas.width, this.canvas.height);  //This is our blanking frame. It is heare that we clear the last image set to the canvas in preperation for the new one to be painted. 
-        ctx.fillStyle = 'black';
-        ctx.fillRect(10, 10, 150, 100);
+    draw () {
+        //This is our blanking frame. It is here that we clear the last image set to the canvas in preperation for the new one to be painted. 
+        this.ctx.clearRect(0,0, this.canvas.width, this.canvas.height);  
+        
+        //Draw the court
+        this.ctx.fillStyle = 'black';
+        this.ctx.fillRect(10, 10, 150, 100);
 
         // Draw the net
-        ctx.fillStyle = "white"
-        ctx.fillRect(400, 0, 2, 600);
-    }
+        this.ctx.fillStyle = "white"
+        this.ctx.fillRect(400, 0, 2, 600);
 
+        this.ball.draw()
+    }
 }
